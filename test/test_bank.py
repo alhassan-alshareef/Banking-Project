ab7527d00da72050cb124884a4b6192b3ade701a
Alhassanalshareff <alhassan.alshareff@gmail.com>
2025-09-24 20:53:40 +0300

    i test my class bank to add new user

--- test/test_bank.py
+++ test/test_bank.py
@@ -8,3 +8,10 @@ class TestBank(unittest.TestCase):
         self.filename = 'banck.csv'
         self.bank = Bank(self.filename)
         
+    def test_add_customer_with_only_checking(self):
+        customer = self.bank.add_new_customer( 'Hassan', 'Ali', 'awq@2341',  1000, 200)
+        self.assertEqual(customer.Fname, 'Hassan') 
+        self.assertEqual(customer.Lname, "Ali") 
+        self.assertEqual(customer.checking_account.balance, 1000) 
+        self.assertEqual(customer.savings_account.balance, 200)
+        self.bank.save_customers()
