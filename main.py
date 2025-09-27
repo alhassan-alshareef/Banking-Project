0a8d2c449362997cfb791868acba7c663ad2dd2f
Alhassanalshareff <alhassan.alshareff@gmail.com>
2025-09-27 19:26:43 +0300

    Initialize BankAPP class for CLI interface with Bank instance

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-def main():
-    while True:
-        print("Welcome to the Banking System!")
-        print("1. Create Account")
-        print("2. Login")
-        print("3. Exit")
-        choice = input("Enter your choice: ")
-        if choice == '1':
-            create_account()
-        elif choice == '2':
-            login()
-        elif choice == '3':
-            print("Thank you for using the Banking System. Goodbye!")
-            break
-        else:
-            print("Invalid input. Please try again.\n")
+from users.users import Bank
+
+
+class BankAPP:
+    def __init__(self, bank):
+        self.bank = bank
+        self.user = None
